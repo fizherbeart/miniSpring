618e4c9b4f07063e08cdd52ca35ca9c5bdff11ad
wzm <23as56df@sina.com>
2024-11-03 23:07:44 +0800

    add exception

---
+++ src/main/java/org/example/exception/BeanNotOfRequiredTypeException.java
@@ -0,0 +1,11 @@
+package org.example.exception;
+
+public class BeanNotOfRequiredTypeException extends BeansException {
+
+    public BeanNotOfRequiredTypeException() {
+    }
+
+    public BeanNotOfRequiredTypeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/exception/NoUniqueBeanDefinitionException.java
@@ -0,0 +1,11 @@
+package org.example.exception;
+
+public class NoUniqueBeanDefinitionException extends BeanDefinitionException {
+
+    public NoUniqueBeanDefinitionException() {
+    }
+
+    public NoUniqueBeanDefinitionException(String message) {
+        super(message);
+    }
+}
